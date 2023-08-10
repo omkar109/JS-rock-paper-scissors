bae1d819f4566993c2d5cfa51718120a9ce432b3
Omkar Kendale <omkarkendale9@gmail.com>
2023-08-10 14:25:01 -0500

    Finished rock paper scissors game

--- script.js
+++ script.js
@@ -14,24 +14,44 @@ function getComputerChoice(){
 function playRound(event){
     playerSelection = event.target.textContent;
     const computerSelection = getComputerChoice();
+    let result;
     if(playerSelection.toLowerCase() == computerSelection.toLowerCase()){
-        return `You tie! You both chose ${playerSelection}`;
+        result = `You tie! You both chose ${playerSelection}`;
     }
     else if(playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper" ||
     playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors" ||
     playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock"){
-        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+        result = `You lose! ${computerSelection} beats ${playerSelection}!`;
+        updateScore("computer");
     }
     else if(playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock" ||
     playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper" ||
     playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors"){
-        return `You win! ${playerSelection} beats ${computerSelection}!`;
+        result = `You win! ${playerSelection} beats ${computerSelection}!`;
+        updateScore("player");
     }
+    results.textContent += result + "\n";
 }
 
 buttons = document.querySelectorAll("button");
-buttons.forEach((button) => button.addEventListener('click', game));
+buttons.forEach((button) => button.addEventListener('click', playRound));
 
-function game(event){
-    console.log(playRound(event));
+results = document.querySelector(".results");
+playerScore = document.querySelector(".playerScore > span")
+computerScore = document.querySelector(".computerScore > span")
+
+function updateScore(winner){
+    if(winner == "player"){
+        playerScore.textContent = parseInt(playerScore.textContent) + 1;
+    }
+    else if(winner == "computer"){
+        computerScore.textContent = parseInt(computerScore.textContent) + 1;
+    }
+    if(parseInt(playerScore.textContent) == 5){
+        alert("Game over. Player Wins!");
+    }
+    
+    if(parseInt(computerScore.textContent) == 5){
+        alert("Game over. Computer Wins!");
+    }
 }
